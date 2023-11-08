5e77d2f10e05d3ceb6bf04a844519d666f49bbe5
Garvit <dhameliyagarvit@gmail.com>
2023-11-08 12:38:12 +0530

    route complate

--- server.js
+++ server.js
@@ -9,10 +9,35 @@
 
 const http = require("http");
 
-const port = 5000;
+// const port = 5000;
 
-http
-  .createServer((req, res) => {
-    console.log("server started");
-  })
-  .listen(port);
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("index success");
+});
+
+app.get("/getuser", (req, res) => {
+  res.status(200).send([
+    {
+      data: "data 1",
+    },
+    {
+      data: "data 2",
+    },
+  ]);
+});
+
+app.post("/createuser", (req, res) => {
+  res.status(201).send({
+    data: "user created",
+  });
+});
+
+// app.listen(5000, (req, res) => {
+//   console.log("server start");
+// });
+
+http.createServer(app).listen(5000);
