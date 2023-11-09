a89cc0937f1732b1672475abd35f71da6a3b35a1
Garvit <dhameliyagarvit@gmail.com>
2023-11-09 12:29:20 +0530

    api done

--- server.js
+++ server.js
@@ -8,36 +8,59 @@
 // fs.writeFileSync("hello.txt", "my name is garvit");
 
 const http = require("http");
-
-// const port = 5000;
-
 const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("index success");
-});
+// const port = 5000;
 
-app.get("/getuser", (req, res) => {
-  res.status(200).send([
-    {
-      data: "data 1",
-    },
-    {
-      data: "data 2",
-    },
-  ]);
-});
+// app.get("/", (req, res) => {
+//   res.status(200).send("index success");
+// });
 
-app.post("/createuser", (req, res) => {
-  res.status(201).send({
-    data: "user created",
-  });
-});
+// app.get("/getuser", (req, res) => {
+//   res.status(200).send([
+//     {
+//       data: "data 1",
+//     },
+//     {
+//       data: "data 2",
+//     },
+//   ]);
+// });
+
+// app.post("/createuser", (req, res) => {
+//   res.status(201).send({
+//     data: "user created",
+//   });
+// });
 
 // app.listen(5000, (req, res) => {
 //   console.log("server start");
 // });
+
+const bodyParser = require("body-parser");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body.value);
+
+  let num1 = parseInt(req.body.num1);
+  let num2 = parseInt(req.body.num2);
+
+  let sum = num1 + num2;
+
+  res.status(200).send({
+    sum: sum,
+  });
+});
 
 http.createServer(app).listen(5000);
